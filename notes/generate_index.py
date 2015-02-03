ac5271034abb6c4996c347e2ad9e95a51df8b5c4
Konstantin Nikitin <stromsund@yandex-team.ru>
2015-02-03 11:44:22 +0300

    Articles are updated

--- notes/generate_index.py
+++ notes/generate_index.py
@@ -11,6 +11,6 @@ with open('README.md', 'w') as readme_file:
     for filename in sorted(f for f in os.listdir('.') if os.path.isfile(f)):
         readme_file.write('* [{0}]({1})\n'.format(filename, urllib.quote_plus(filename)))
 
-
+subprocess.check_call(['/bin/sh', '-c', 'markdown README.md > index.html'])
 subprocess.check_call(['git', 'add', '-A', '.'])
 subprocess.check_call(['git', 'commit', '-m', 'Articles are updated'])
